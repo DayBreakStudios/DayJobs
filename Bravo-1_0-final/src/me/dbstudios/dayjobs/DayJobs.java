ec7d1f3ef17783b5cd8cb7c586a8e6575bb44c92
Tyler Lartonoix <larttyler@live.com>
2011-09-04 21:06:40 -0400

    Version 1.0 final source

---
+++ Bravo-1_0-final/src/me/dbstudios/dayjobs/DayJobs.java
@@ -0,0 +1,324 @@
+package me.dbstudios.dayjobs;
+
+import java.io.File;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Event;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.config.Configuration;
+import org.bukkit.plugin.Plugin;
+
+import com.nijiko.permissions.PermissionHandler;
+import com.nijikokun.bukkit.Permissions.Permissions;
+
+
+
+public class DayJobs extends JavaPlugin {
+	// Private variables
+	public String pluginDir = "plugins" + File.separator + "dbstudios" + File.separator + "DayJobs" + File.separator;
+	private Logger log = Logger.getLogger("Minecraft");
+	private String version = "1.0";
+	private static PermissionHandler PermHandler;
+	public String prefix = "<DayJobs> ";
+	private Boolean debug = false;
+	
+	// Configuration links
+	private Configuration config = new Configuration(new File(pluginDir + "config.yml"));
+	private Configuration players = new Configuration(new File(pluginDir + "player.yml"));
+	private Configuration ticket = new Configuration(new File(pluginDir + "ticket.yml"));
+	
+	//Listeners
+	//private final DayJobsBlockListener blockListener = new DayJobsBlockListener(this);
+	private final DayJobsPlayerListener playerListener = new DayJobsPlayerListener(this);
+		
+	@Override
+	public void onEnable() {
+		// Register listeners
+	 	PluginManager manager = this.getServer().getPluginManager();
+		//manager.registerEvent(Event.Type.BLOCK_PLACE, blockListener, Event.Priority.Normal, this);
+		manager.registerEvent(Event.Type.PLAYER_JOIN, playerListener, Event.Priority.Normal, this);
+		manager.registerEvent(Event.Type.PLAYER_PICKUP_ITEM, playerListener, Event.Priority.Normal, this);
+		manager.registerEvent(Event.Type.PLAYER_INTERACT, playerListener, Event.Priority.Normal, this);
+		
+		//Initialize Permissions
+		setupPerms();
+						
+		config.load();
+		players.load();
+		ticket.load();
+		
+		if (config.getString("config.enabled") == "false") {
+			log.info(prefix + "Plugin disabled (Reason: config.enabled set to 'false').");
+		}
+		
+		if (config.getString("config.debug") == "true") {
+			log.info(prefix + "Verbose logging enabled.");
+			debug = true;
+		}
+		
+		log.info(prefix + "Plugin version " + version + " enabled.");
+	}
+	
+	@Override
+	public void onDisable() {
+		log.info(prefix + "Plugin disabled.");
+	}
+	
+	public void setupPerms() {
+		if (PermHandler != null) {
+			return;
+		}
+		
+		Plugin PermPlug = this.getServer().getPluginManager().getPlugin("Permissions");
+		
+		if (PermPlug == null) {
+			log.info("Permissions not found, defaulting to OP.");
+			return;
+		}
+		
+		PermHandler = ((Permissions)PermPlug).getHandler();
+		log.info("Permissions found, using " + ((Permissions)PermPlug).getDescription().getFullName());
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		return (new DayJobsCommands(this)).tryCommand(sender, cmd, label, args);
+	}
+	
+	/* *** COMMON *** */
+	public Boolean checkMatch(String block, String player, String type) {
+		/**
+		 * Description:
+		 * 	block	- The block (or item) in hand/being placed
+		 * 	player	- the player to perform the check on
+		 * 	type	- the type of check to perform (i.e. 'can-place', 'can-wear', 'can-use')
+		 * 
+		 * So, for example, to check if player "LartTyler" can place the block "DIRT", one would call:
+		 * 	checkMatch("DIRT", this.getPlayer("LartTyler"), "can-place");
+		 */
+		
+		Boolean matched = false;
+		block = block.replace("[", "").replace("]", "");
+		
+		String job = getJob(player);
+		String[] toCheck = parseToList(getConfig("jobs." + job + "." + type));
+		
+		for (String item : toCheck) {
+			ifDebug("Checking '" + item + "' against placed block '" + block + "'.");
+			
+			if (item.equalsIgnoreCase(block)) {
+				matched = true;
+				break;
+			}
+		}
+		
+		if (!matched) {
+			toCheck = parseToList(getConfig("all." + type));
+			for (String item : toCheck) {
+				if (item.equalsIgnoreCase(block)) {
+					matched = true;
+					break;
+				}
+			}
+		}
+		
+		ifDebug("CheckMatch returning '" + matched.toString() + "'.");
+		return matched;
+	}
+	
+	public String[] parseToList(String toParse) {
+		return (toParse.replace("[", "").replace("]", "").replace(" ", "").split(","));
+	}
+	
+	public List<String> getJobList() {
+		/**
+		 * Returns a List<String> of all jobs available to the player
+		 */
+
+		return config.getKeys("config.jobs");
+	}
+
+	public String getInfo(String job, String node) {
+		/**
+		 * Returns a job info node.
+		 */
+
+		return config.getString("config.jobs." + job + "." + node);
+	}
+
+	public Boolean createTicket(String subject, String time, String job) {
+		/**
+		 * Attempts to create a new ticket in pluginDir + "ticket.yml"
+		 * Will return true on success, false if the write failed
+		 */
+
+		String base = "ticket." + subject;
+
+		ticket.setProperty(base + ".time", time);
+		ticket.setProperty(base + ".job", job);
+		ticket.save();
+
+		return (getTicket(subject, "job") == job);
+	}
+
+	public String getJob(String player) {
+		/**
+		 * Returns <player>'s current job class
+		 */
+
+		return players.getString("player." + player + ".job");
+	}
+
+	public String parseToLine(String toParse) {
+		/**
+		 * Parses a YAML value list to a more visually appealing single line list
+		 */
+
+		return (toParse.replace("[", "").replace("]", "").replace(",", ", ").replace("_", " "));
+	}
+
+	public Boolean hasPerm(String player, String perm) {
+		/**
+		 * Returns true if <player> has the correct <perm> node, false if not
+		 */
+		perm = "dbstudios.dayjobs." + perm;
+
+		return (PermHandler.has(this.getServer().getPlayer(player), perm) || this.getServer().getPlayer(player).isOp());
+	}
+
+	public void reloadConf() {
+		/**
+		 * Attempt to reload all configuration files
+		 */
+
+		config.load();
+		ticket.load();
+		players.load();
+	}
+
+	public List<String> getOpenTickets() {
+		/**
+		 * Return a List<String> of all open tickets
+		 */
+
+		return (ticket.getKeys("ticket"));
+	}
+
+	public String getTicket(String subject, String node) {
+		/**
+		 * Attempts to get the specified <node> from the ticket made by <subject>
+		 */
+
+		return ticket.getString("ticket." + subject + "." + node);
+	}
+
+	public Boolean playerExists(String player) {
+		/**
+		 * Return true if <player>'s job is found, false if not (he doesn't exist)
+		 */
+
+		return (getJob(player) != null);
+	}
+
+	public Boolean jobExists(String job) {
+		/**
+		 * Return true if <job> can be found in config.yml, false if not
+		 */
+
+		return (config.getString("config.jobs." + job + ".friendly-name") != null);
+	}
+
+	public Boolean changeJob(String player, String job) {
+		/**
+		 * Attempts to change <player>'s job to <job>
+		 * Will return true on success, false otherwise
+		 */
+
+		if (playerExists(player) && jobExists(job)) {
+			players.setProperty("player." + player + ".job", job);
+			players.save();
+
+			return (getJob(player) == job);
+		} else {
+			return false;
+		}
+	}
+
+	public Boolean ticketExists(String subject) {
+		/**
+		 * Return true if a ticket has been opened by <subject>
+		 */
+
+		return (getTicket(subject, "time") != null);
+	}
+
+	public Boolean closeTicket(String subject) {
+		/**
+		 * Attempts to close ticket opened by <subject>
+		 */
+
+		if (ticketExists(subject)) {
+			ticket.removeProperty("ticket." + subject);
+			ticket.save();
+			return (ticket.getProperty("ticket." + subject) == null);
+		} else {
+			return false;
+		}
+	}
+	
+	public String getConfig(String opt) {
+		/**
+		 * Load <opt> from config.yml
+		 */
+		
+		return config.getString("config." + opt);
+	}
+	
+	public Boolean writeConfig(String opt, String val) {
+		/**
+		 * Attempt to write <opt>: <val> to config.yml
+		 * Will return true on success, else returns false
+		 */
+		
+		config.setProperty("config." + opt, val);
+		config.save();
+		
+		reloadConf();
+		
+		return (config.getString("config." + opt) == val);
+	}
+	
+	public Boolean createPlayer(String player, String job) {
+		/**
+		 * Attempt to create <player> and add them into player.yml
+		 */
+		
+		players.setProperty("player." + player + ".job", job);
+		players.save();
+		
+		return (getJob(player) != null);
+	}
+	
+	public void ifDebug(String msg) {
+		/**
+		 * Output additional debug info to the console if the correct configuration node is set
+		 */
+		
+		if (debug) {
+			log.info(prefix + msg);
+		}
+	}
+	
+	public Boolean toggleDebug() {
+		/**
+		 * Toggle debug mode
+		 */
+		
+		debug = !debug;	
+		return true;
+	}
+}
---
+++ Bravo-1_0-final/src/me/dbstudios/dayjobs/DayJobsCommands.java
@@ -0,0 +1,205 @@
+package me.dbstudios.dayjobs;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class DayJobsCommands {
+	private DayJobs common;
+	
+	public DayJobsCommands(DayJobs instance) {
+		common = instance;
+	}
+	
+	public Boolean tryCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (label.equalsIgnoreCase("job")) {
+			switch (args.length) {
+				case 1:
+					if (args[0].equalsIgnoreCase("list") && common.hasPerm(((Player)sender).getDisplayName(), "player.list")) {
+						List<String> jobs = common.getJobList();
+
+						sender.sendMessage(common.prefix + "Available jobs:");
+						if (jobs == null) {
+							sender.sendMessage(ChatColor.AQUA + "--> No jobs available.");
+						} else {
+							for (String job : jobs) {
+								String fname = common.getInfo(job, "friendly-name");
+
+								sender.sendMessage(ChatColor.AQUA + "--> " + job + " (" + fname + ")");
+							}
+						}
+
+						return true;
+					} else if (args[0].equalsIgnoreCase("info") && common.hasPerm(((Player)sender).getDisplayName(), "player.info")) {
+						String job = common.parseToLine(common.getJob(((Player)sender).getDisplayName()));
+						String bio = common.parseToLine(common.getInfo(job, "bio"));
+						String fname = common.parseToLine(common.getInfo(job, "friendly-name"));
+						String canPlace = common.parseToLine(common.getInfo(job, "can-place"));
+						String canUse = common.parseToLine(common.getInfo(job, "can-use"));
+						//String canWear = common.parseToLine(common.getInfo(job, "can-wear"));
+						
+						if (canPlace.length() == 0) {
+							canPlace = "Nothing";
+						}
+						if (canUse.length() == 0) {
+							canUse = "Nothing";
+						}
+						//if (canWear.length() == 0) {
+						//	canWear = "Nothing";
+						//}
+
+						sender.sendMessage(common.prefix + "Job info for '" + job + "'");
+						sender.sendMessage(ChatColor.AQUA + "--> Name: " + ChatColor.GREEN + fname);
+						sender.sendMessage(ChatColor.AQUA + "--> Bio: " + ChatColor.GREEN + bio);
+						sender.sendMessage(ChatColor.AQUA + "--> Can place: " + ChatColor.GREEN + canPlace);
+						sender.sendMessage(ChatColor.AQUA + "--> Can use: " + ChatColor.GREEN + canUse);
+						//sender.sendMessage(ChatColor.AQUA + "--> Can wear: " + ChatColor.GREEN + canWear);
+
+						return true;
+					} else {
+						return false;
+					}
+					
+				case 2:
+					if (args[0].equalsIgnoreCase("change") && common.hasPerm(((Player)sender).getDisplayName(), "player.change")) {
+						if (common.jobExists(args[1])) {
+							String time = (new SimpleDateFormat("M/d @ h:mm a").format(Calendar.getInstance().getTime()));
+
+							if (common.createTicket(((Player)sender).getDisplayName(), time, args[1])) {
+								sender.sendMessage(common.prefix + ChatColor.AQUA + "Change request submitted successfully.");
+							} else {
+								sender.sendMessage(common.prefix + ChatColor.RED + "Error submitting ticket...");
+							}
+
+							return true;
+						}
+					} else if (args[0].equalsIgnoreCase("whois") && common.hasPerm(((Player)sender).getDisplayName(), "player.whois")) {
+						String job = common.getJob(args[1]);
+
+						if (job != null) {
+							sender.sendMessage(common.prefix + "Whois lookup for '" + args[1] + "':"); // <-- Happy face 'cuz we found them!
+							sender.sendMessage(ChatColor.AQUA + "--> Job: " + ChatColor.GREEN + job);
+						} else {
+							sender.sendMessage(common.prefix + ChatColor.RED + "Whois failed: " +
+									ChatColor.DARK_AQUA + "Player '" + args[1] + "' does not exist."); // <-- sad face 'cuz we didn't...
+						}
+
+						return true;
+					} else if (args[0].equalsIgnoreCase("info") && common.hasPerm(((Player)sender).getDisplayName(), "player.info")) {
+						if (common.jobExists(args[1])) {
+							String job = args[1];
+							String bio = common.parseToLine(common.getInfo(job, "bio"));
+							String fname = common.parseToLine(common.getInfo(job, "friendly-name"));
+							String canPlace = common.parseToLine(common.getInfo(job, "can-place"));
+							String canUse = common.parseToLine(common.getInfo(job, "can-use"));
+							//String canWear = common.parseToLine(common.getInfo(job, "can-wear"));
+							
+							if (canPlace.length() == 0) {
+								canPlace = "Nothing";
+							}
+							if (canUse.length() == 0) {
+								canUse = "Nothing";
+							}
+							//if (canWear.length() == 0) {
+							//	canWear = "Nothing";
+							//}
+	
+							sender.sendMessage(common.prefix + "Job info for '" + job + "'");
+							sender.sendMessage(ChatColor.AQUA + "--> Name: " + ChatColor.GREEN + fname);
+							sender.sendMessage(ChatColor.AQUA + "--> Bio: " + ChatColor.GREEN + bio);
+							sender.sendMessage(ChatColor.AQUA + "--> Can place: " + ChatColor.GREEN + canPlace);
+							sender.sendMessage(ChatColor.AQUA + "--> Can use: " + ChatColor.GREEN + canUse);
+							//sender.sendMessage(ChatColor.AQUA + "--> Can wear: " + ChatColor.GREEN + canWear);
+						} else {
+							sender.sendMessage(common.prefix + ChatColor.RED + "Error: " + ChatColor.DARK_AQUA + "Job '" + args[1] + "' does not exist.");
+						}
+	
+						return true;
+					} else if (args[0].equalsIgnoreCase("admin")) {
+						if (args[1].equalsIgnoreCase("reload") && common.hasPerm(((Player)sender).getDisplayName(), "admin.reload")) {
+							common.reloadConf();
+							common.ifDebug("YAMLs reloaded by '" + ((Player)sender).getDisplayName());
+							sender.sendMessage(common.prefix + ChatColor.AQUA + "Reload successful.");
+							
+							return true;
+						} else if (args[1].equalsIgnoreCase("tickets") && common.hasPerm(((Player)sender).getDisplayName(), "admin.tickets")) {
+							List<String> tickets = common.getOpenTickets();
+	
+							sender.sendMessage(common.prefix + "Open tickets:");
+							if (tickets == null) {
+								sender.sendMessage(ChatColor.AQUA + "--> None!");
+							} else {
+								for (String subject : tickets) {
+									String time = common.getTicket(subject, "time");
+									String job = common.getTicket(subject, "job");
+	
+									sender.sendMessage(ChatColor.AQUA + "--> (" + time + ") '" + subject + "' to '" + job + "'");
+								}
+							}
+	
+							return true;
+						} else if (args[1].equalsIgnoreCase("debug") && common.hasPerm(((Player)sender).getDisplayName(), "admin.debug")) {
+							common.ifDebug("Player '" + ((Player)sender).getDisplayName() + "' toggled debug mode.");
+							common.toggleDebug();
+						} else {
+							return false;
+						}
+					}else {
+						return false;
+					}
+
+			case 4:
+				if (args[0].equalsIgnoreCase("admin")) {
+					if (args[1].equalsIgnoreCase("change") && common.hasPerm(((Player)sender).getDisplayName(), "admin.change")) {
+						String player = args[2];
+						String job = args[3];
+
+						if (common.playerExists(player) && common.jobExists(job)) {
+							if (common.changeJob(player, job)) {
+								sender.sendMessage(common.prefix +
+									"Successfully changed '" + player +
+									"' to '" + job + "'");
+							} else {
+								sender.sendMessage(common.prefix + ChatColor.RED + "Error: " +
+										ChatColor.DARK_AQUA + "Could not change '" + player + "' to '" + job + "'...");
+							}
+						} else if (!common.playerExists(player)) {
+							sender.sendMessage(common.prefix + ChatColor.RED + "Error: " + ChatColor.DARK_AQUA +
+									"Player '" + player + "' does not exist.");
+						} else if (!common.jobExists(job)) {
+							sender.sendMessage(common.prefix + ChatColor.RED + "Error: " + ChatColor.DARK_AQUA +
+									"Job '" + job + "' does not exist.");
+						}
+						
+						return true;
+					} else if (args[1].equalsIgnoreCase("tickets") && args[2].equalsIgnoreCase("close") && common.hasPerm(((Player)sender).getDisplayName(), "admin.tickets")) {
+						String ticket = args[3];
+						
+						if (common.ticketExists(ticket)) {
+							if (common.closeTicket(ticket)) {
+								sender.sendMessage(common.prefix + ChatColor.AQUA +"Ticket for '" + ticket + "' closed successfully.");
+							} else {
+								sender.sendMessage(common.prefix + ChatColor.RED + "Error: " + ChatColor.DARK_AQUA +
+										"Could not close ticket for '" + ticket + "'...");
+							}
+						} else {
+							sender.sendMessage(common.prefix + ChatColor.AQUA + "No open tickets for '" + ticket + "'.");
+						}
+						
+						return true;
+					} else {
+						return false;
+					}
+				}
+			}
+		}
+		
+		return false;
+	}
+}
+
