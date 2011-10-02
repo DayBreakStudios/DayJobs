4556caf6442d407952c40c60ca72ac11f4ab5e26
Tyler Lartonoix <larttyler@live.com>
2011-10-02 10:18:38 -0400

    Version 2.0 final source

---
+++ Charlie-2_0-final/src/me/dbstudios/dayjobs/DJBlockListener.java
@@ -0,0 +1,36 @@
+package me.dbstudios.dayjobs;
+
+import org.bukkit.ChatColor;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockListener;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+public class DJBlockListener extends BlockListener {
+	DayJobs common;
+	
+	public DJBlockListener(DayJobs instance) {
+		common = instance;
+	}
+	
+	@Override
+	public void onBlockPlace(BlockPlaceEvent ev) {
+		String block = ev.getBlock().getType().name();
+		String player = ev.getPlayer().getDisplayName();
+		
+		if (!common.checkMatch(block, player, "can-place") && !common.isExempt(player)) {
+			ev.setCancelled(true);
+			ev.getPlayer().sendMessage(common.prefix + ChatColor.DARK_AQUA + common.getPlaceDenyMsg(player));
+		}
+	}
+	
+	@Override
+	public void onBlockBreak(BlockBreakEvent ev) {
+		String block = ev.getBlock().getType().name();
+		String player = ev.getPlayer().getDisplayName();
+		
+		if (!common.checkMatch(block, player, "can-break") && !common.isExempt(player)) {
+			ev.setCancelled(true);
+			ev.getPlayer().sendMessage(common.prefix + ChatColor.DARK_AQUA + common.getBreakDenyMsg(player));
+		}
+	}
+}
